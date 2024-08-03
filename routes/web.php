aa4b117199f8281e2d45a2de4429acb098abbedc
paulholsters <paul.holsters@hotmail.com>
2024-08-03 12:57:03 +0200

    init

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('tests',[
+        'tests' => [
+            ['charter'=>'charter A
+             flutftfktfkutfkutflutf tfktfkutfkutflutfkut
+             flutftfktfkutfkutflutf tfktfkutfkutflutfkut
+             flutftfktfkutfkutflutf tfktfkutfkutflutfkut tfktfkutfkutflutftfktfkutfkutflutf tfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+            ['charter'=>'charter B tfktfkutfkutflutftfktfkutfkutflutftfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+            ['charter'=>'charter N tfktfkutfkutflutftfktfkutfkutflutftfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+            ['charter'=>'charter N tfktfkutfkutflutftfktfkutfkutflutftfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+            ['charter'=>'charter N tfktfkutfkutflutftfktfkutfkutflutftfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+            ['charter'=>'charter N tfktfkutfkutflutftfktfkutfkutflutftfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+            ['charter'=>'charter N tfktfkutfkutflutftfktfkutfkutflutftfktfkutfkutflutfkutfkutflutf','date'=>'5/6/2024'],
+        ]
+    ]);
+});
+
+Route::get('/{id}/test', function () {
+    return view('test');
+});
+
+Route::get('/bugreport', function () {
+    return view('bugreport');
+});
